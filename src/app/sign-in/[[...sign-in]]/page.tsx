426ddb89b34e7dc4aa81e4e3671386b5baaaa1f8
NelTeano <jonelteano29@gmail.com>
2024-10-15 13:03:25 +0800

    add aichatbot

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { Component } from 'react'
 import { SignIn } from '@clerk/nextjs'
+import Script from 'next/script';
 
 
 export default class page extends Component {
@@ -8,6 +9,13 @@ export default class page extends Component {
     return (
         <div className='flex h-[500px] justify-center items-center bg-white'>
           <SignIn fallbackRedirectUrl="/Home" />
+           <Script
+              id="chatling-embed-script"
+              strategy="afterInteractive"
+              src="https://chatling.ai/js/embed.js"
+              async
+              data-id="4358988268"
+            />
         </div>
     )
   }
